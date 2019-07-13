bf1a0d4e3899179e1631ca16a90cb0d527d40e37
Srikesh <srikesh.nichenametla@oracle.com>
2019-07-13 20:46:56 +0530

    more input validation

--- src/com/srikesh/converter/Main.java
+++ src/com/srikesh/converter/Main.java
@@ -8,10 +8,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Please Enter the number between [0 - 999999999] to convert into words");
-       Integer numberToBeConverted = StringUtils.sanitize(input.nextLine());
-       input.close();
+		System.out.println("Enter the number between [0 - 999999999] to convert into words");
+       try {
+		Integer numberToBeConverted = StringUtils.sanitize(input.nextLine());
        System.out.println(new NumberToWordsConverter().convertToWords(numberToBeConverted));
+       }
+       catch (Exception e) {
+    	   System.out.println(e.getMessage());
+       }
+       finally {
+		input.close();
+	}
 	}
 
 }
--- src/com/srikesh/converter/utils/StringUtils.java
+++ src/com/srikesh/converter/utils/StringUtils.java
@@ -3,6 +3,9 @@ package com.srikesh.converter.utils;
 public class StringUtils {
 
 	public static Integer sanitize(String input) {
-		return Integer.parseInt(input.replace(",", ""));
+		input = input.replace(",", "");
+		if(input.length() > 9 || input.contains("."))
+			throw new RuntimeException("unsupported number, supported range is 0 - 999999999 and without decimals");
+		return Integer.parseInt(input);
 	}
 }
